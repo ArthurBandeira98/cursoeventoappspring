038f5be06e1f775619c91d68e8cd6ab2d6a8e9b4
Arthur Bandeira <arthur.baandeira@gmail.com>
2022-08-16 21:49:58 -0300

    corrigindo projeto

--- src/main/java/com/cursoeventoappspring/DataConfigurantion.java
+++ src/main/java/com/cursoeventoappspring/DataConfigurantion.java
@@ -1,35 +1,33 @@
 package com.cursoeventoappspring;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import javax.xml.crypto.URIReferenceException;
-
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class DataConfigurantion {
 
-	@Bean
-	public BasicDataSource dataSource() throws URIReferenceException, URISyntaxException {
-
-		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-		String username = dbUri.getUserInfo().split(":")[0];
-		String password = dbUri.getUserInfo().split(":")[1];
-		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath()
-				+ "?sslmode=require";
-
-		BasicDataSource basicDataSource = new BasicDataSource();
-		basicDataSource.setUrl(dbUrl);
-		basicDataSource.setUsername(username);
-		basicDataSource.setPassword(password);
-
-		return basicDataSource;
-
-	}
+//	@Bean
+//	public BasicDataSource dataSource() throws URIReferenceException, URISyntaxException {
+//
+//		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+//
+//		String username = dbUri.getUserInfo().split(":")[0];
+//		String password = dbUri.getUserInfo().split(":")[1];
+//		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath()
+//				+ "?sslmode=require";
+//
+//		BasicDataSource basicDataSource = new BasicDataSource();
+//		basicDataSource.setUrl(dbUrl);
+//		basicDataSource.setUsername(username);
+//		basicDataSource.setPassword(password);
+//
+//		return basicDataSource;
+//
+//	}
 
 	// criando tabelas auto
 	// @Bean
@@ -47,27 +45,27 @@ public class DataConfigurantion {
 
 	// ?useTimezone=true&serverTimezone=UTC
 
-	// @Bean
-	// public DataSource dataSource() {
-	// DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	// dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	// dataSource.setUrl("jdbc:mysql://localhost:3306/eventospring?useTimezone=true&serverTimezone=UTC");
-	// dataSource.setUsername("root");
-	// dataSource.setPassword("admin");
-	// return dataSource;
-	//
-	// }
-	//
-	// @Bean
-	// public JpaVendorAdapter jpaVendorAdapter() {
-	// HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-	// adapter.setDatabase(Database.MYSQL);
-	// adapter.setShowSql(true);
-	// adapter.setGenerateDdl(true);
-	// adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
-	// adapter.setPrepareConnection(true);
-	// return adapter;
-	//
-	// }
+	@Bean
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/eventospring?useTimezone=true&serverTimezone=UTC");
+		dataSource.setUsername("root");
+		dataSource.setPassword("admin");
+		return dataSource;
+
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+
+	}
 
 }
